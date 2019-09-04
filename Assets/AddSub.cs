af0b04e733e6ce4787c6da0ca9e264990219bbde
Ben Tristem <ben@bentristem.com>
2019-09-04 17:00:24 +0100

    Using checked keyword for integer overflow protection

---
+++ Assets/AddSub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSub : MonoBehaviour {
+
+    int bigNumber = 2147483640;
+
+    // Use this for initialization
+    void Start()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(bigNumber);
+        bigNumber = checked(bigNumber + 1);
+	}
+}
