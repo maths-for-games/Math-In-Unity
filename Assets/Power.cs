11fb498440cabd9ed3f3846ef843defb4a76b3d2
Ben Tristem <ben@bentristem.com>
2019-09-19 18:59:54 +0100

    6_IA_MFG

---
+++ Assets/Power.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Mathf.Pow(2, 24));
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
